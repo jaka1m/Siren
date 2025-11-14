09d29b63b08ba2da33b35e790c4a960d8a834850
jaka1m <61670480+jaka1m@users.noreply.github.com>
2025-11-14 19:04:52 +0800

    Update vmess.rs

--- src/proxy/vmess.rs
+++ src/proxy/vmess.rs
@@ -12,15 +12,20 @@ use md5::{Digest, Md5};
 use sha2::Sha256;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use worker::*;
+use uuid::Uuid;
+
+struct VmessCommand {
+    is_tcp: bool,
+    remote_port: u16,
+    remote_addr: String,
+    key: [u8; 16],
+    iv: [u8; 16],
+    options: [u8; 4],
+}
 
 
 impl <'a> ProxyStream<'a> {
     async fn aead_decrypt(&mut self) -> Result<Vec<u8>> {
-        let key = crate::md5!(
-            &self.config.uuid.as_bytes(),
-            b"c48619fe-8f02-49e0-b9e9-edf763e17e21"
-        );
-
         // +-------------------+-------------------+-------------------+
         // |     Auth ID       |   Header Length   |       Nonce       |
         // +-------------------+-------------------+-------------------+
@@ -28,23 +33,29 @@ impl <'a> ProxyStream<'a> {
         // +-------------------+-------------------+-------------------+
         let mut auth_id = [0u8; 16];
         self.read_exact(&mut auth_id).await?;
-        let mut len = [0u8; 18];
-        self.read_exact(&mut len).await?;
+        let mut len_buf = [0u8; 18];
+        self.read_exact(&mut len_buf).await?;
         let mut nonce = [0u8; 8];
         self.read_exact(&mut nonce).await?;
 
-        // https://github.com/v2fly/v2ray-core/blob/master/proxy/vmess/aead/kdf.go
-        let header_length = {
+        let mut key: Option<[u8; 16]> = None;
+        let mut header_length: u16 = 0;
+
+        for uuid in &self.config.uuid {
+            let p_key = crate::md5!(uuid.as_bytes());
+
+            // https://github.com/v2fly/v2ray-core/blob/master/proxy/vmess/aead/kdf.go
             let header_length_key = &hash::kdf(
-                &key,
+                &p_key,
                 &[
                     KDFSALT_CONST_VMESS_HEADER_PAYLOAD_LENGTH_AEAD_KEY,
                     &auth_id,
                     &nonce,
                 ],
             )[..16];
+
             let header_length_nonce = &hash::kdf(
-                &key,
+                &p_key,
                 &[
                     KDFSALT_CONST_VMESS_HEADER_PAYLOAD_LENGTH_AEAD_IV,
                     &auth_id,
@@ -53,16 +64,19 @@ impl <'a> ProxyStream<'a> {
             )[..12];
 
             let payload = Payload {
-                msg: &len,
+                msg: &len_buf,
                 aad: &auth_id,
             };
 
-            let len = Aes128Gcm::new(header_length_key.into())
-                .decrypt(header_length_nonce.into(), payload)
-                .map_err(|e| Error::RustError(e.to_string()))?;
+            if let Ok(len) = Aes128Gcm::new(header_length_key.into())
+                .decrypt(header_length_nonce.into(), payload) {
+                header_length = ((len[0] as u16) << 8) | (len[1] as u16);
+                key = Some(p_key.into());
+                break;
+            }
+        }
 
-            ((len[0] as u16) << 8) | (len[1] as u16)
-        };
+        let key = key.ok_or(Error::RustError("can not find available uuid".to_string()))?;
 
         // 16 bytes padding
         let mut cmd = vec![0u8; (header_length + 16) as _];
@@ -95,8 +109,8 @@ impl <'a> ProxyStream<'a> {
         Ok(header_payload)
     }
 
-    pub async fn process_vmess(&mut self) -> Result<()> {
-        let mut buf = Cursor::new(self.aead_decrypt().await?);
+    async fn parse_vmess_command(&mut self, p: Vec<u8>) -> Result<VmessCommand> {
+        let mut buf = Cursor::new(p);
 
         // https://xtls.github.io/en/development/protocols/vmess.html#command-section
         //
@@ -126,9 +140,23 @@ impl <'a> ProxyStream<'a> {
         let remote_port = parse_port(&mut buf).await?;
         let remote_addr = parse_addr(&mut buf).await?;
 
+        Ok(VmessCommand {
+            is_tcp,
+            remote_port,
+            remote_addr,
+            key,
+            iv,
+            options,
+        })
+    }
+
+    pub async fn process_vmess(&mut self) -> Result<()> {
+        let p = self.aead_decrypt().await?;
+        let cmd = self.parse_vmess_command(p).await?;
+
         // encrypt payload
-        let key = &crate::sha256!(&key)[..16];
-        let iv = &crate::sha256!(&iv)[..16];
+        let key = &crate::sha256!(&cmd.key)[..16];
+        let iv = &crate::sha256!(&cmd.iv)[..12];
 
         // https://github.com/v2ray/v2ray-core/blob/master/proxy/vmess/encoding/client.go#L196
         let length_key = &hash::kdf(&key, &[KDFSALT_CONST_AEAD_RESP_HEADER_LEN_KEY])[..16];
@@ -143,7 +171,7 @@ impl <'a> ProxyStream<'a> {
         let payload_iv = &hash::kdf(&iv, &[KDFSALT_CONST_AEAD_RESP_HEADER_IV])[..12];
         let header = {
             let header = [
-                options[0], // https://github.com/v2ray/v2ray-core/blob/master/proxy/vmess/encoding/client.go#L242
+                cmd.options[0], // https://github.com/v2ray/v2ray-core/blob/master/proxy/vmess/encoding/client.go#L242
                 0x00, 0x00, 0x00,
             ];
             Aes128Gcm::new(payload_key.into())
@@ -152,9 +180,9 @@ impl <'a> ProxyStream<'a> {
         };
         self.write(&header).await?;
 
-        if is_tcp {
+        if cmd.is_tcp {
             let addr_pool = [
-                (remote_addr.clone(), remote_port),
+                (cmd.remote_addr.clone(), cmd.remote_port),
                 (self.config.proxy_addr.clone(), self.config.proxy_port)
             ];
 
