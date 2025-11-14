06b405a27d8d06c874f4b845372fc0cb99d2c86c
jaka1m <61670480+jaka1m@users.noreply.github.com>
2025-11-14 19:27:33 +0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -18,12 +18,9 @@ static PROXYKV_PATTERN: Lazy<Regex> = Lazy::new(|| Regex::new(r"^([A-Z]{2})").un
 
 #[event(fetch)]
 async fn main(req: Request, env: Env, _: Context) -> Result<Response> {
-    let uuid = env.var("UUID").map(|x| {
-        x.to_string()
-            .split(',')
-            .filter_map(|x| Uuid::parse_str(x.trim()).ok())
-            .collect()
-    })?;
+    let uuid = env
+        .var("UUID")
+        .map(|x| Uuid::parse_str(&x.to_string()).unwrap_or_default())?;
     let host = req.url()?.host().map(|x| x.to_string()).unwrap_or_default();
     let main_page_url = env.var("MAIN_PAGE_URL").map(|x| x.to_string()).unwrap();
     let sub_page_url = env.var("SUB_PAGE_URL").map(|x| x.to_string()).unwrap();
